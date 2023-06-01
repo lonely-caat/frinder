d25bb6d730eaa7a583d87a88df7b5ca3a5d2969f
lonely-caat <maxbilichenko@gmail.com>
2023-06-01 14:53:48 +0300

    removing comment

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,18 +1,3 @@
-// import express, { json } from 'express';
-//
-// const app = express();
-//
-// app.use(json())
-//
-// const PORT = process.env.PORT || 3000;
-//
-// app.get('/', async (req, res) => {
-//     res.json({ status: true, message: "Our node.js app works" })
-// });
-//
-// app.listen(PORT, () => console.log(`App listening at port ${PORT}`));
-//
-
 import express from 'express';
 import ProfileRoutes from './routes/profileRoutes.js';
 
@@ -24,4 +9,3 @@ app.use('/api/Profiles', ProfileRoutes);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`App listening at port ${PORT}`));
-export default app;
